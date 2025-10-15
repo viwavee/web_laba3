18404c43dde930850343202a6de40c7f26778a67
Polina <polinawey2@gmail.com>
2025-10-15 12:32:00 +0200

    Подготовка формы для PHP

--- www/js/script.js
+++ www/js/script.js
@@ -1,16 +1,10 @@
 document.getElementById("libraryForm").addEventListener("submit", function(e) {
-  e.preventDefault();
-
-  const formData = new FormData(this);
-  let output = "<h3>Результат:</h3>";
-
-  for (const [name, value] of formData.entries()) {
-    output += `<p><b>${name}:</b> ${value}</p>`;
-  }
-
-  if (!formData.has("ebook")) {
-    output += "<p><b>Электронная версия:</b> нет</p>";
-  }
-
-  document.getElementById("result").innerHTML = output;
+    
+    const username = document.querySelector("[name='username']").value;
+    const ticket = document.querySelector("[name='ticket']").value;
+    const genre = document.querySelector("[name='genre']").value;
+    const ebook = document.querySelector("[name='ebook']").checked ? "да" : "нет";
+    const period = document.querySelector("[name='period']").value;
+    
+    alert("Вы ввели:\nИмя: " + username + "\nНомер билета: " + ticket + "\nЖанр: " + genre + "\nЭлектронная: " + ebook + "\nСрок: " + period);
 });
